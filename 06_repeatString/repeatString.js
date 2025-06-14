25a6698cb905a3cf7ac496a050c48f1e6d546d0d
MrDotss <nunezclifford24@gmail.com>
2025-06-14 19:21:41 +0800

    repeat string using loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num >= 0) {
+        const word = str;
+        str = ""
+       for (let i = 1; i <= num; i++) {
+            str+= word
+       }
 
+        return str;
+
+    } else return "ERROR"
 };
 
 // Do not edit below this line
