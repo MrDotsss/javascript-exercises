8fb628c49a1401213dd679a99d073c4208b0438b
MrDotsss <nunezclifford24@gmail.com>
2025-07-19 07:59:22 +0800

    All test cases passed used reduce method and for loops

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  if (numbers.length === 0) return 0;
+
+  return numbers.reduce((acc, val) => acc + val);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((acc, val) => acc * val);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1
+  if (n === 1 || n === 0) return result;
+
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+
 };
 
 // Do not edit below this line
