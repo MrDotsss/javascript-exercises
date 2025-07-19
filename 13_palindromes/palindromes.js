a51d807d2bea3bbb30433a662a339431ace1aba5
MrDotsss <nunezclifford24@gmail.com>
2025-07-19 09:47:47 +0800

    All test success reversed the string and used regex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reversed = word.trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+        .split('').reverse().join('');
 
+    return reversed.replace(/\s/g, '')
+        .toLowerCase() === word.trim()
+            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+            .replace(/\s/g, '').toLowerCase();
 };
-
 // Do not edit below this line
 module.exports = palindromes;
