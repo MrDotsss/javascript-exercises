a4f30eeabd25de9ddde42ec99f108982beacae93
MrDotsss <nunezclifford24@gmail.com>
2025-07-19 10:06:37 +0800

    Simple iteration, getting value from array of obj

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let newBooks = []
 
+    for (let i = 0; i < array.length; i++) {
+        newBooks[i] = array[i].title
+    }
+    return newBooks;
 };
 
 // Do not edit below this line
